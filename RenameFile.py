182703d89f243b0349b68246d2c0a8c7ea1e1fbe
hulsebuschb <hulsebuschb@gmail.com>
2021-05-23 12:14:51 -0500

    fixing function names (gif to mp4)

--- RenameFile.py
+++ RenameFile.py
@@ -3,15 +3,15 @@
 import cv2
 
 
-class GifReverser:
+class MP4Reverser:
     global filename 
     filename = 'FILENAMEHERE'  # replace your filename here! DO NOT INCLUDE THE EXTENSION
     # DO NOT INCLUDE THE EXTENSION
     def __init__(self):
         pass
 
-    def get_frames(gif_filename: str):
-        vidcap = cv2.VideoCapture(gif_filename)
+    def get_frames(mp4_filename: str):
+        vidcap = cv2.VideoCapture(mp4_filename)
         framerate = vidcap.get(cv2.CAP_PROP_FPS)
         frames = []
 
@@ -21,9 +21,8 @@ class GifReverser:
             success, image = vidcap.read()
         return (framerate, frames)
 
-    # also works with mp4 files
-    def reverse_gif(gif_filename: str, output_filename: str):
-        framerate, frames = GifReverser.get_frames(gif_filename)
+    def reverse_mp4(mp4_filename: str, output_filename: str):
+        framerate, frames = MP4Reverser.get_frames(mp4_filename)
         height, width, layers = frames[0].shape
         size = (width, height)
         print('Your output file: ' + output_filename)
@@ -42,5 +41,5 @@ if __name__ == '__main__':
     reversed_filename = filename + '_r'
     file = filename + '.mp4'
     reversed_file = reversed_filename + '.mp4'
-    GifReverser.reverse_gif(file, reversed_file);
+    MP4Reverser.reverse_mp4(file, reversed_file);
     print('Done!')
